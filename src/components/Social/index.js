aa2619e6ae851de60549f6cbb3e9a846c59d727c
Eric da Costa <ericdacosta72@gmail.com>
2022-11-13 14:37:32 -0300

    Cria componente de social media

---
+++ src/components/Social/index.js
@@ -0,0 +1,7 @@
+import "./social.css"
+
+export function Social({url, children }){
+    return(
+        <a className="social" href={url} rel="noopener noreferrer" target="_blank">{children}</a>
+    )
+}
